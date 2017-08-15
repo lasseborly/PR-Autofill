3b71b5d0cb5d8cec2221595fc13e6a35d841f340
Lasse Borly <lasseborly@gmail.com>
2017-08-15 19:48:43 +0200

    add proper template, version bump and auto zip

--- public/injectPRTemplate.js
+++ public/injectPRTemplate.js
@@ -1,23 +1,18 @@
+const template =
+`#### What does this PR do?
+#### Where should the reviewer start?
+#### How should this be (manually) tested?
+#### Any background context you want to provide?
+#### What are the relevant tickets? (Optional)
+#### Screenshots (if appropriate)
+#### Questions:`
+
 chrome.runtime.onMessage.addListener(action => {
     const textArea = document.getElementById('id_description')
-    const title = document.getElementById('id_title')
 
     if (action === 'inject') {
-
       if (textArea && textArea.value === '') {
-        textArea.value =
-  `#### What does this PR do?
-  #### Where should the reviewer start?
-  #### How should this be (manually) tested?
-  #### Any background context you want to provide?
-  #### What are the relevant tickets? (Optional)
-  #### Screenshots (if appropriate)
-  #### Questions:
-    - `
-      }
-
-      if (title && title === '') {
-        title.value = 'Feature/'
+        textArea.value = template
       }
     }
 });
